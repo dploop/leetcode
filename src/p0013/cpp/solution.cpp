b2e0bd6866187c6bd3a55a0d5d34201e4db02549
Qiang Liu <dploop@163.com>
2019-12-20 18:47:51 +0800

    leetcode p0013

--- src/p0013/cpp/solution.cpp
+++ src/p0013/cpp/solution.cpp
@@ -1,14 +1,8 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-
 class Solution {
 public:
     int romanToInt(const string &s) {
         int result = 0, prev = 0, curr;
-        for (auto i = s.size(); i--; prev = curr) {
+        for (size_t i = s.size(); i--; prev = curr) {
             curr = c2n(s[i]);
             if (curr < prev) {
                 result -= curr;
@@ -20,17 +14,24 @@ public:
     }
 
 private:
-    int c2n(char c) {
+    static int c2n(char c) {
         switch (c) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            default:  return 0;
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
         }
     }
 };
-
--- src/p0013/golang/solution.go
+++ src/p0013/golang/solution.go
@@ -1,19 +1,8 @@
-package main
-
 func romanToInt(s string) int {
-	c2n := map[byte]int{
-		'I': 1,
-		'V': 5,
-		'X': 10,
-		'L': 50,
-		'C': 100,
-		'D': 500,
-		'M': 1000,
-	}
 	result, prev, curr := 0, 0, 0
 	for i := len(s); i > 0; prev = curr {
 		i--
-		curr = c2n[s[i]]
+		curr = c2n(s[i])
 		if curr < prev {
 			result -= curr
 		} else {
@@ -21,4 +10,25 @@ func romanToInt(s string) int {
 		}
 	}
 	return result
+}
+
+func c2n(c byte) int {
+	switch c {
+	case 'I':
+		return 1
+	case 'V':
+		return 5
+	case 'X':
+		return 10
+	case 'L':
+		return 50
+	case 'C':
+		return 100
+	case 'D':
+		return 500
+	case 'M':
+		return 1000
+	default:
+		return 0
+	}
 }
