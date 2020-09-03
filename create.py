eb02c8d657eefbda84f9579def6386c447a09f87
Qiang Liu <dploop@163.com>
2020-09-03 12:56:21 +0800

    refactor

--- create.py
+++ create.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
     index = int(sys.argv[1])
     url = r'https://leetcode.com/api/problems/all/'
     headers = {
-        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:47.0) Gecko/20100101 Firefox/47.0',
-        'Content-Type':'application/json'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:47.0) Gecko/20100101 Firefox/47.0',
+        'Content-Type': 'application/json'
     }
-    req = urllib.request.Request(url=url,headers=headers)
+    req = urllib.request.Request(url=url, headers=headers)
     res = json.loads(urllib.request.urlopen(req).read().decode('utf-8'))
     for obj in res['stat_status_pairs']:
         if obj['stat']['frontend_question_id'] != index:
@@ -22,10 +22,10 @@ if __name__ == '__main__':
         if not os.path.exists(sub):
             os.mkdir(sub)
         attr = {
-            "addition": "",
-            'id':index,
-            'title':title,
-            'url':'https://leetcode.com/problems/%s/' % slug
+            'addition': '',
+            'id': index,
+            'title': title,
+            'url': 'https://leetcode.com/problems/%s/' % slug
         }
         fo = open(os.path.join(sub, 'attr.json'), 'w')
         json.dump(attr, fo, indent=4)
--- readme.py
+++ readme.py
@@ -1,60 +1,68 @@
 # -*- coding:utf-8 -*-
-import os, json
+import json
+import os
 from urllib.parse import quote
 from pipe import *
 
-def langs(problem):
-    prob_exts = os.listdir(problem)
-    prob_lang = []
-    configs = [
-        ('C',      ['c']),
-        ('C++',    ['cpp', 'cc']),
-        ('Go',     ['golang', 'go']),
-        ('Java',   ['java']),
-        ('JS',     ['javascript', 'js']),
-        ('Python', ['python', 'py']),
-        ('Rust',   ['rust', 'rs']),
-        ('SQL',    ['sql']),
-        ('Shell',  ['shell']),
-    ]
-    for (lang, exts) in configs:
-        for ext in exts:
-            if ext in prob_exts:
-                prob_lang.append(lang)
-                break
-    return '/'.join(prob_lang)
+# 语言及其对应子目录名称
+language_subs = [
+    ('C', ['c']),
+    ('C++', ['cpp', 'cc']),
+    ('Go', ['golang', 'go']),
+    ('Java', ['java']),
+    ('JS', ['javascript', 'js']),
+    ('Python', ['python', 'py']),
+    ('Rust', ['rust', 'rs']),
+    ('SQL', ['sql']),
+    ('Shell', ['shell']),
+]
 
+
+# 获取问题对应的语言列表
+def get_languages(problem):
+    directories = os.listdir(problem)
+    languages = []
+    for (language, subs) in language_subs:
+        if len(set(subs) & set(directories)):
+            languages.append(language)
+    return '/'.join(languages)
+
+
+# 获取问题对应的整行数据
 def get_row(problem):
     basename = os.path.basename(problem)
     attr = json.load(open(os.path.join(problem, 'attr.json')))
-    index = '%04d' % int(attr.get('id', '0'))
+    number = '%04d' % int(attr.get('id', '0'))
     title = '[%s]( %s )' % (attr.get('title', ''), attr.get('url', ''))
-    solution = '[%s]( src/%s )' % (langs(problem), quote(basename))
+    solution = '[%s]( src/%s )' % (get_languages(problem), quote(basename))
     # favorite: ⭐
     # complete: ✅
     addition = attr.get('addition', '')
-    return (index, title, addition, solution)
+    return number, title, addition, solution
 
+
+# 格式化整行数据
 def fmt_row(row):
     return '| %s | %s %s | %s |' % row
 
+
 if __name__ == '__main__':
     (__file__
-        | Pipe(os.path.realpath)
-        | Pipe(os.path.dirname)
-        | Pipe(os.chdir)
-    )
-    ofile = open('README.md', 'w')
-    ofile.write('# LeetCode Solutions\n\n')
-    ofile.write('| Index | Title | Solution |\n')
-    ofile.write('| :---: | :---- | :------: |\n')
+     | Pipe(os.path.realpath)
+     | Pipe(os.path.dirname)
+     | Pipe(os.chdir)
+     )
+    outfile = open('README.md', 'w')
+    outfile.write('# LeetCode Solutions\n\n')
+    outfile.write('| Index | Title | Solution |\n')
+    outfile.write('| :---: | :---- | :------: |\n')
     (os.listdir('src')
-        | select(lambda p: os.path.join('src', p))
-        | where(os.path.isdir)
-        | select(get_row)
-        | sort
-        | select(fmt_row)
-        | Pipe('\n'.join)
-        | Pipe(ofile.write)
-        | Pipe(lambda _: print('Done!'))
-    )
+     | select(lambda p: os.path.join('src', p))
+     | where(os.path.isdir)
+     | select(get_row)
+     | sort
+     | select(fmt_row)
+     | Pipe('\n'.join)
+     | Pipe(outfile.write)
+     | Pipe(lambda _: print('Done!'))
+     )
