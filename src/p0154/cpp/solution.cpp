78abd664cb75ef4f3ba4097a320e78fe49a99cc0
Qiang Liu <dploop@163.com>
2021-01-23 13:14:49 +0800

    leetcode p0153 & p0154

--- src/p0154/cpp/solution.cpp
+++ src/p0154/cpp/solution.cpp
@@ -6,13 +6,14 @@ using namespace std;
 
 class Solution {
 public:
-    int findMin(vector<int> &nums) {
-        size_t begin = 0, end = nums.size(); if (end == 0) return 0;
-        while (begin < end && nums[begin] == nums[end - 1]) end--;
-        while (begin < end) {
-            auto mid = begin + (end - begin) / 2;
-            (nums[mid] >= nums[0]) ? (begin = mid + 1): (end = mid);
+    int findMin(vector<int> &array) {
+        size_t l = 0, r = array.size();
+        for (int back = array[--r]; l < r;) {
+            size_t m = l + (r - l) / 2;
+            if (array[l] == back) l++;
+            else if (array[m] <= back) r = m;
+            else l = m + 1;
         }
-        return (end == nums.size()) ? nums[0] : nums[end];
+        return array[l];
     }
 };
